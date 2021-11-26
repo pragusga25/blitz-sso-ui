49620040dd2aebe919876c1fda964a4e9f8c509a
Taufik Pragusga <73622464+pragusga25@users.noreply.github.com>
2021-11-26 10:02:56 +0700

    Update index.ts

--- app/auth/config/index.ts
+++ app/auth/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
   CAS_URL: "https://sso.ui.ac.id/cas2/",
-  SERVICE_URL: process.env.SERVICE_URL || "http://localhost:3000/api/auth/sso-ui",
+  SERVICE_URL: process.env.BLITZ_PUBLIC_SERVICE_URL || "http://localhost:3000/api/auth/sso-ui",
   PATHNAME: "/api/auth/sso-ui",
 }
